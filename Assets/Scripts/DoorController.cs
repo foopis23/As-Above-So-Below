3da4be783355b6f84988f786b11471162c052921
Eric Sexton <foopis23@gmail.com>
2022-01-30 15:46:23 -0500

    hello world

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using FMOD;
 using UnityEngine;
 
 public class DoorController : MonoBehaviour
@@ -12,20 +13,31 @@ public class DoorController : MonoBehaviour
     public string doorCloseSoundName;
 
     private FMODHelper _fmodHelper;
+    private bool _isOpen = false;
 
     private void Start()
     {
         _fmodHelper = new FMODHelper(new string[]{doorOpenSoundName, doorCloseSoundName});
+        _fmodHelper.FMODEvents[doorCloseSoundName]
+            .set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
+        
+        _fmodHelper.FMODEvents[doorOpenSoundName]
+            .set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
     }
     
     public void OpenDoor()
     {
+        if (_isOpen) return;
+        _isOpen = true;
         doorAnimator.Play(openAnimationName);
         _fmodHelper.PlayOneshot(doorOpenSoundName);
     }
 
     public void CloseDoor()
     {
+        if (!_isOpen) return;
+        _isOpen = false;
+        
         doorAnimator.Play(closeAnimationName);
         _fmodHelper.PlayOneshot(doorCloseSoundName);
     }
