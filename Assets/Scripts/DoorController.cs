c3fdc90763fbc325d2710313dccfe833400bce90
Eric Sexton <foopis23@gmail.com>
2022-01-30 11:34:20 -0500

    add some door ness

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -8,13 +8,25 @@ public class DoorController : MonoBehaviour
     public string openAnimationName;
     public string closeAnimationName;
     
+    public string doorOpenSoundName;
+    public string doorCloseSoundName;
+
+    private FMODHelper _fmodHelper;
+
+    private void Start()
+    {
+        _fmodHelper = new FMODHelper(new string[]{doorOpenSoundName, doorCloseSoundName});
+    }
+    
     public void OpenDoor()
     {
         doorAnimator.Play(openAnimationName);
+        _fmodHelper.PlayOneshot(doorOpenSoundName);
     }
 
     public void CloseDoor()
     {
         doorAnimator.Play(closeAnimationName);
+        _fmodHelper.PlayOneshot(doorCloseSoundName);
     }
 }
